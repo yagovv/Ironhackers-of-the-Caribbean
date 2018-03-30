590fcf068d82364281e19526f220ff14e4346021
Yago Vega <yagofvv@gmail.com>
2018-03-30 22:04:06 +0200

    grid added

--- game.js
+++ game.js
@@ -1,17 +1,55 @@
-function Game (canvas, ctx) {
+function Game(canvas, ctx) {
   this.canvas = canvas;
   this.ctx = ctx;
   this.player = new Component(this.canvas, this.ctx);
 }
 
 Game.prototype.start = function() {
-  this.id = setInterval(function() {
-    this.clear();
-    this.player.draw();
-    //draws
-  }.bind(this),17);
-}
+  this.id = setInterval(
+    function() {
+      this.clear();
+      this.player.draw();
+      this.drawGrid();
+      this.player.handleKeys();
+      //draws
+    }.bind(this),
+    17
+  );
+};
 
-Game.prototype.clear = function () {
+Game.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
+};
+
+Game.prototype.drawGrid = function() {
+  var minor = 10;
+  var major = minor * 5;
+  var stroke = "#00FF00";
+  var fill = "#009900";
+  this.ctx.save();
+  this.ctx.strokeStyle = stroke;
+  this.ctx.fillStyle = fill;
+  let width = this.ctx.canvas.width;
+  let height = this.ctx.canvas.height;
+  for (var x = 0; x < width; x += minor) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, 0);
+    this.ctx.lineTo(x, height);
+    this.ctx.lineWidth = x % major == 0 ? 0.5 : 0.25;
+    this.ctx.stroke();
+    if (x % major == 0) {
+      this.ctx.fillText(x, x, 10);
+    }
+  }
+  for (var y = 0; y < height; y += minor) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, y);
+    this.ctx.lineTo(width, y);
+    this.ctx.lineWidth = y % major == 0 ? 0.5 : 0.25;
+    this.ctx.stroke();
+    if (y % major == 0) {
+      this.ctx.fillText(y, 0, y + 10);
+    }
+  }
+  this.ctx.restore();
+};
--- objects.js
+++ objects.js
@@ -1,21 +1,26 @@
 function Component(canvas, ctx) {
-  this.width = 40;
-  this.height = 40;
+  this.width = 50;
+  this.height = 50;
   this.x = 0;
   this.y = canvas.height - this.height;
   this.canvas = canvas;
   this.ctx = ctx;
   this.speedX = 1;
   this.speedY = 1;
-
-  document.onkeydown = function (event) {
-    var RIGHT_KEY = 39;
-    var LEFT_KEY = 37;
-    var UP_KEY = 40;
-    var DOWN_KEY = 38;
+}
+Component.prototype.handleKeys = function() {
+  var RIGHT_KEY = 39;
+  var LEFT_KEY = 37;
+  var UP_KEY = 40;
+  var DOWN_KEY = 38;
+  document.onkeypress = function(event) {
     switch (event.keyCode) {
       case RIGHT_KEY:
-        this.moveRight();
+        console.log("right key pressed");
+        this.id = setInterval(function() {
+          this.moveRight();
+          console.log("moving right");
+        }, 1000 / this.speedX);
         break;
       case LEFT_KEY:
         this.moveLeft();
@@ -28,9 +33,26 @@ function Component(canvas, ctx) {
         break;
     }
   }.bind(this);
-}
+  document.onkeyup = function(event) {
+    switch (event.keyCode) {
+      case RIGHT_KEY:
+        clearInterval(this.id);
+        break;
+      case LEFT_KEY:
+        this.moveLeft();
+        break;
+      case UP_KEY:
+        this.moveUp();
+        break;
+      case DOWN_KEY:
+        this.moveDown();
+        break;
+    }
+  }.bind(this);
+};
+
 Component.prototype.draw = function() {
-  this.ctx.fillStyle = 'red';
+  this.ctx.fillStyle = "red";
   this.ctx.fillRect(this.x, this.y, this.width, this.height);
 };
 Component.prototype.moveRight = function() {
@@ -57,9 +79,3 @@ Component.prototype.accelerateY = function() {
 Component.prototype.brakeY = function() {
   this.speedY--;
 };
-
-function Player(color, x, y) {
-  this.color = color;
-  this.x = x;
-  this.y = y;
-}
