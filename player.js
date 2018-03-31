f92c8928fcbca9a79d4db12a1b5dd1c7fc6643f8
Yago Vega <yagofvv@gmail.com>
2018-03-31 14:12:34 +0200

    new structure of objects

---
+++ player.js
@@ -0,0 +1,60 @@
+function Player(canvas, ctx) {
+    this.width = 50;
+    this.height = 50;
+    this.x = 0;
+    this.y = canvas.height - this.height;
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.speedX = 5;
+    this.speedY = 5;
+}
+Player.prototype.handleKeys = function () {
+    var RIGHT_KEY = 39;
+    var LEFT_KEY = 37;
+    var UP_KEY = 40;
+    var DOWN_KEY = 38;
+    document.onkeydown = function (event) {
+        switch (event.keyCode) {
+            case RIGHT_KEY:
+                this.moveRight();
+                break;
+            case LEFT_KEY:
+                this.moveLeft();
+                break;
+            case UP_KEY:
+                this.moveUp();
+                break;
+            case DOWN_KEY:
+                this.moveDown();
+                break;
+        }
+    }.bind(this);
+}
+Player.prototype.draw = function () {
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+Player.prototype.moveRight = function () {
+    this.x += this.speedX;
+};
+Player.prototype.moveLeft = function () {
+    this.x -= this.speedX;
+};
+Player.prototype.moveUp = function () {
+    this.y += this.speedY;
+};
+Player.prototype.moveDown = function () {
+    this.y -= this.speedY;
+};
+Player.prototype.accelerateX = function () {
+    this.speedX++;
+};
+Player.prototype.brakeX = function () {
+    this.speedX--;
+};
+Player.prototype.accelerateY = function () {
+    this.speedY++;
+};
+Player.prototype.brakeY = function () {
+    this.speedY--;
+};
